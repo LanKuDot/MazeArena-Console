6a703a70af9bd7993961c6e53b988eaa5843de07
LanKuDot <airlanser@gmail.com>
2018-07-05 00:07:29 +0800

    ColorPosFinder: Find and mark the target color

--- color_position_finder.py
+++ color_position_finder.py
@@ -3,6 +3,8 @@ Find the position of the specified color in the image.
 """
 
 import cv2
+import imutils
+import numpy as np
 
 class ColorPosition:
 	"""Data structure storing the position of the color found in the frame
@@ -52,6 +54,10 @@ class ColorPositionFinder:
 				break
 
 			self._frame = self._camera.get_frame()
+
+			if len(self.colors_to_find) > 0:
+				self.find_colors()
+
 			cv2.imshow(windowName, self._frame)
 
 		cv2.destroyWindow(windowName)
@@ -76,3 +82,41 @@ class ColorPositionFinder:
 		self.colors_to_find.append(ColorPosition([color_r, color_g, color_b]))
 		print("[ColorPositionFinder] New target color added:" \
 			" ({0}, {1}, {2})".format(color_r, color_g, color_b))
+
+	def find_colors(self):
+		# Convert color from RGB domain from HSV domain
+		frame_hsv = cv2.cvtColor(self._frame, cv2.COLOR_BGR2HSV)
+		target_color_hsv = cv2.cvtColor(np.uint8([[self.colors_to_find[0].color]]), cv2.COLOR_BGR2HSV)
+
+		# Set the range of detecting colors in HSV domain
+		hue = target_color_hsv[0][0][0]
+		low_hue = hue - 15 if hue - 15 > -1 else 0
+		high_hue = hue + 15 if hue + 15 < 256 else 255
+		lower_bound = np.array([low_hue, 100, 100], dtype = np.uint8)
+		upper_bound = np.array([high_hue, 255, 255], dtype = np.uint8)
+
+		# Only colors in defined range will be passed
+		filtered_frame = cv2.inRange(frame_hsv, lower_bound, upper_bound)
+
+		# Erode and dilate the filtered result with 7 x 7 kernal
+		# to eliminate the noise
+		kernal = np.ones((7, 7), dtype = np.uint8)
+		filtered_frame = cv2.erode(filtered_frame, kernal, iterations = 1)
+		filtered_frame = cv2.dilate(filtered_frame, kernal, iterations = 1)
+		filtered_frame = cv2.GaussianBlur(filtered_frame, (7, 7), 0)
+
+		# Find contours in the final filtered frame
+		contours = cv2.findContours(filtered_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		contours = contours[0] if imutils.is_cv2() else contours[1]
+
+		# Find center point of each contour
+		centres = []
+		for i in range(len(contours)):
+			moments = cv2.moments(contours[i])
+			centres.append((int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00'])))
+
+		# Draw the dot found
+		for i in range(len(centres)):
+			cv2.circle(self._frame, centres[i], 5, (0, 0, 150), -1)
+
+		#return self.colors_to_find
