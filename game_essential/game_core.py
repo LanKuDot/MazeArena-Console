d3e79a4ae9ac6484dcd7ad11f9d855358c169b92
LanKuDot <airlanser@gmail.com>
2018-10-06 16:17:03 +0800

    BasicGameCore: Add communication server as member

--- game_essential/game_core.py
+++ game_essential/game_core.py
@@ -10,6 +10,7 @@ class BasicGameCore:
 		maze_manager: MazeManager):
 		self._player_info_table = player_info_table
 		self._maze_manager = maze_manager
+		self._comm_server = comm_server
 
 		self._is_game_started = False
 
@@ -17,10 +18,9 @@ class BasicGameCore:
 	def is_game_started(self):
 		return self._is_game_started
 
-	def _register_command(self):
-		comm_server.set_new_connection_handler(self.player_join)
-		comm_server.set_disconnection_handler(self.player_quit)
-		comm_server.add_command_handler("register", self.player_register_ID)
+	def register_command(self):
+		self._comm_server.set_disconnection_handler(self.player_quit)
+		self._comm_server.add_command_handler("join", self.player_join)
 
 	def player_join(self, player_ip):
 		self._player_info_table.add_player_info(player_ip)
@@ -38,12 +38,12 @@ class BasicGameCore:
 		if self._is_game_started:
 			return
 
-		comm_server.boardcast_message("server game-start")
+		self._comm_server.boardcast_message("server game-start")
 		self._is_game_started = True
 
 	def game_stop(self):
 		if not self._is_game_started:
 			return
 
-		comm_server.boardcast_message("server game-stop")
+		self._comm_server.boardcast_message("server game-stop")
 		self._is_game_started = False
