f7e049447a3801adc723fb227ea097f15efb969d
LanKuDot <airlanser@gmail.com>
2018-10-07 11:21:17 +0800

    PlayerInfoTable: Rename _player_info_table to _table

--- game_essential/player_info_table.py
+++ game_essential/player_info_table.py
@@ -10,22 +10,22 @@ class BasicPlayerInfo:
 class BasicPlayerInfoTable:
 	def __init__(self, player_info_T = BasicPlayerInfo):
 		self._player_info_T = player_info_T
-		self._player_info_table = {}
+		self._table = {}
 
 	def add_player_info(self, player_ip, player_ID):
 		new_player_info = self._player_info_T()
 		new_player_info.IP = player_ip
 		target_info.ID = player_ID
-		self._player_info_table[player_ip] = new_player_info
+		self._table[player_ip] = new_player_info
 
 	def delete_player_info(self, player_ip):
 		target_player_info = self.get_player_info_by_IP(player_ip)
 
 		if target_player_info is not None:
-			self._player_info_table.pop(player_ip)
+			self._table.pop(player_ip)
 
 	def get_player_info_by_IP(self, player_ip) -> BasicPlayerInfo:
-		return self._player_info_table.get(player_ip)
+		return self._table.get(player_ip)
 
 	def set_player_color(self, player_ip, color_bgr):
 		"""Set the LED color of the player
