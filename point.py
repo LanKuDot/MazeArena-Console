0c884ee4aadffaa55bd086a506003fba9c8d2f9c
LanKuDot <airlanser@gmail.com>
2018-10-29 23:12:50 +0800

    Point2D: Add add and minus operation

--- point.py
+++ point.py
@@ -15,6 +15,12 @@ class Point2D(namedtuple('Point2D', 'x y')):
 	def __repr__(self):
 		return "Point2D({0}, {1})".format(self.x, self.y)
 
+	def __add__(self, other):
+		return Point2D(self.x + other.x, self.y + other.y)
+
+	def __sub__(self, other):
+		return Point2D(self.x - other.x, self.y - other.y)
+
 	@staticmethod
 	def distance(a, b):
 		return math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
